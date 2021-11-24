4fe3996c03e93fa7ca6be67391d815354f695255
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2021-11-24 19:41:45 +0200

    Working with conditional rendering

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const app = Vue.createApp({
     data(){
         return {
+            displayBooks: true,
             title: 'Kath Way City',
             author: 'Zack Mutombo',
             age: 35,
-            likes: 20+'k'
+            likes: 20+'k',
+            btn: 'Hide Books'
         }
     },
     methods: {
-        updateTitle: function(){
-            this.title = "I am a man in the misor"
-        },
-        changeAuthor(author){
-            this.author = author
+        toogleBooks(){
+            this.displayBooks = !this.displayBooks;
+            if(this.btn === 'Hide Books') {
+                this.btn = 'Show Books'
+            } else {
+                this.btn = 'Hide Books'
+            }
         }
     }
 });

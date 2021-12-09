a0ba2983600e464d26adf86c97d7c9fcc62e83b7
Katshi Mutombo <katshi.mutombo@sanlam.co.za>
2021-12-09 17:35:01 +0200

    Toogle the fav class

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const app = Vue.createApp({
     data(){
         return {
-            displayBooks: true,
-            title: 'Kath Way City',
-            author: 'Zack Mutombo',
-            age: 35,
-            likes: 20+'k',
-            btn: 'Hide Books',
+            books: [
+                {title: 'A la croisee de chemin', author: 'Zack', img: 'assets/1.jpg', isFav: true},
+                {title: 'Une nuit de carnavale', author: 'Kath', img: 'assets/2.jpg', isFav: false},
+                {title: 'Quand tu n\'est plus la', author: 'Neron', img: 'assets/1.jpg', isFav: true},
+            ]
         }
     },
     methods: {
-        toogleBooks(){
-            this.displayBooks = !this.displayBooks;
-            if(this.btn === 'Hide Books') {
-                this.btn = 'Show Books'
-            } else {
-                this.btn = 'Hide Books'
-            }
-        },
+        toogleBtn(book){
+            // if(book.isFav === true){
+            //     book.isFav = false
+            // } else {
+            //     book.isFav = true
+            // }
+            book.isFav = !book.isFav
+        }
     }
 });
 
